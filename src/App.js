55604b3c7c7d74395fa5045f2a406e6e0b3895d2
pbson <phambason639@gmail.com>
2023-10-12 10:04:36 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NQueensSolver from './components/NQueensSolver';
+
+function App() {
+  return (
+    <div className="App">
+      <NQueensSolver />
+    </div>
+  );
+}
+
+export default App;
